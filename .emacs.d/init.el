cc0260794663802980312647cff98cfb79ff01de
V Sreekanth <sreeix@gmail.com>
2013-12-15 06:00:59 +0530

    Setting up super binding to right alt.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,6 +45,7 @@
 
 (global-set-key (kbd "C-c h") 'helm-projectile)
 (global-set-key (kbd "C-c m") 'magit-status)
+(setq ns-right-option-modifier ‘super)
 (yas-global-mode 1)
 ;;(load-theme 'solarized-light)
 (load-theme 'wombat)
