8968f37cdbf80d8b1427820274d98662fcc56675
sreekanth <sreekanth@Sreekanths-MacBook-Pro.local>
2014-09-23 10:31:48 +0530

    Update most of the stuff again.
    Adding osx.sh into the mix

--- .emacs.d/elpa/flycheck-tip-20131025.320/flycheck-tip-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "flycheck-tip" "20131025.320" "show flycheck's error by popup-tip" (quote ((flycheck "0.13") (dash "1.2") (emacs "24.1") (popup "0.5.0"))))
--- .emacs.d/elpa/magit-filenotify-20131018.1053/magit-filenotify-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "magit-filenotify" "20131018.1053" "Refresh status buffer when git tree changes [github]" (quote ((magit "1.3.0"))))
