622a5337991857053f29b0ebdb187e9e2056348e
V Sreekanth <sreeix@gmail.com>
2014-12-06 23:25:38 +0530

    Removed commented stuff

--- .emacs.d/lisp/betterdefaults.el
+++ .emacs.d/lisp/betterdefaults.el
@@ -2,7 +2,7 @@
   (ido-mode t)
   (setq ido-enable-flex-matching t)
 
- ;; (menu-bar-mode -1)
+  (menu-bar-mode -1)
   (when (fboundp 'tool-bar-mode)
     (tool-bar-mode -1))
   (when (fboundp 'scroll-bar-mode)
@@ -22,16 +22,19 @@
   (global-set-key (kbd "C-M-s") 'isearch-forward)
   (global-set-key (kbd "C-M-r") 'isearch-backward)
 
+  (global-set-key (kbd "C-x C-b") 'ibuffer)
+  (global-set-key (kbd "M-z") 'zap-up-to-char)
+
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
   (setq x-select-enable-clipboard t
         x-select-enable-primary t
         save-interprogram-paste-before-kill t
         apropos-do-all t
+        visible-bell t
         mouse-yank-at-point t
         save-place-file (concat user-emacs-directory "places")
         backup-directory-alist `(("." . ,(concat user-emacs-directory
                                                  "backups")))))
 
 (provide 'better-defaults)
-
