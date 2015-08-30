ef0623602cf801d2a264e25bad58039920c37b34
V Sreekanth <sreeix@gmail.com>
2015-08-30 20:43:29 +0530

    Update package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,7 +58,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((zencoding- (insert) ndentation . 8) (sgml-basic-offset . 8) (js2-strict-inconsistent-return-warning))))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote cabal-repl))
+ '(haskell-tags-on-save t)
+ '(magit-commit-arguments nil)
+ '(safe-local-variable-values
+   (quote
+    ((zencoding-
+      (insert)
+      indentation . 8)
+     (sgml-basic-offset . 8)
+     (js2-strict-inconsistent-return-warning))))
  '(show-paren-mode t)
-
  '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ .emacs.d/lisp/setup-keybindings.el
@@ -0,0 +1,4 @@
+;; Key bindings.
+
+(global-set-key (kbd "C-c h") 'helm-projectile)
+(global-set-key (kbd "C-c m") 'magit-status)
